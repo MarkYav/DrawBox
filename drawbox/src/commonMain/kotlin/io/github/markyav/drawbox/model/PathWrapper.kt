fd62629b33f91c763cad3a933be37fd804a33c8f
Mark Yavorskyi <yavorskiymark@gmail.com>
2023-04-01 19:14:38 +0300

    Added PathWrapper

--- drawbox/src/commonMain/kotlin/io/github/markyav/drawbox/model/PathWrapper.kt
+++ drawbox/src/commonMain/kotlin/io/github/markyav/drawbox/model/PathWrapper.kt
@@ -1,4 +1,12 @@
 package io.github.markyav.drawbox.model
 
-class PathWrapper {
-}
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+
+data class PathWrapper(
+    var points: SnapshotStateList<Offset>,
+    val strokeWidth: Float = 5f,
+    val strokeColor: Color,
+    val alpha: Float = 1f
+)
