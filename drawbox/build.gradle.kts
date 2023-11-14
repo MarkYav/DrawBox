c009dba19e04931f3b15fde382cfb058914f686a
Mark Yavorskyi <yavorskiymark@gmail.com>
2023-11-14 15:37:49 +0100

    Upgraded: - updated Compose Multiplatform to 1.5.10 - updated compile and target SDK to 34 - updated Kotlin to 1.9.20

--- drawbox/build.gradle.kts
+++ drawbox/build.gradle.kts
@@ -9,7 +9,12 @@ group = Library.group
 version = Library.version
 
 kotlin {
-    android {
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
         publishLibraryVariants("release")
     }
     jvm("desktop") {
@@ -22,11 +27,8 @@ kotlin {
                 api(compose.foundation)
             }
         }
-        val commonTest by getting
         val androidMain by getting
-        val androidTest by getting
         val desktopMain by getting
-        val desktopTest by getting
     }
 }
 
